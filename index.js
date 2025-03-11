e7e8cee87049a0ed4c8f0bf9ab74d97c496aa065
BHAINKAR <anishlado9@gmail.com>
2025-03-11 08:01:26 +0545

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["📺 Watching SpireMC", "🎮 Playing SpireMC"];
+const statusMessages = ["📺 Watching 𝐍𝐞𝐩𝐂𝐫𝐚𝐟𝐭", "🎮 Playing 𝐍𝐞𝐩𝐂𝐫𝐚𝐟𝐭"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
